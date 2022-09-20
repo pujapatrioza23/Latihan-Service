0ed46504ff916872fa6e85340427c47fa47e8e9a
pujapatrioza23 <113494064+pujapatrioza23@users.noreply.github.com>
2022-09-20 13:50:05 +0700

    Add files via upload
    jdk-17.0.4.1
    Spring boot 2.7.3
    Packaging jar

---
+++ Latihan-service/src/test/java/com/pujapatrioza/Latihanservice/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pujapatrioza.Latihanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
